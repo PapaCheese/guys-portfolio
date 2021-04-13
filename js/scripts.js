83c5394b705b9a868457649bc96a98a617dc2b0a
Guy Dadon <guydadon14@gmail.com>
2021-04-13 20:41:11 +0300

    fiverr link

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,7 @@
 
         $('html, body').animate({
             scrollTop: scrollDistance + 'px'
-        }, Math.abs(window.pageYOffset - $(heading).offset().top) / 1);
+        }, Math.abs(window.pageYOffset - $(heading).offset().top) / 4);
 
         // Hide the menu once clicked if mobile
         if ($('header').hasClass('active')) {
